6161b98646186cc6f721c1435a2af320cbec4f37
pius <piusmanandu@gmail.com>
2025-07-11 18:21:18 +0300

    Update and rename config.env to main.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "HansTz&21xwXBBY#kXmlEd8D45Z89lX8nbFBTvG6gStbny5lvAwxXYCK3SM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
